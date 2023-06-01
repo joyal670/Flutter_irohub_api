3a2228c4ebf8192f346ea3e13c51d93db2e81661
Joyal <joyal670@gmail.com>
2023-06-01 11:33:08 +0530

    fixed listing

--- lib/screens/product_item.dart
+++ lib/screens/product_item.dart
@@ -21,50 +21,52 @@ class ProductItem extends StatelessWidget {
       child: InkWell(
         onTap: () {},
         child: Container(
-          alignment: Alignment.topCenter,
+          alignment: Alignment.center,
           decoration: BoxDecoration(
             border: Border.all(style: BorderStyle.none),
           ),
-          child: Column(
-            children: [
-              SizedBox(
-                height: 100,
-                child: Image.network(pImage),
-              ),
-              Text(
-                pName,
-                style: const TextStyle(
-                    color: Colors.black,
-                    fontSize: 20,
-                    fontStyle: FontStyle.normal,
-                    fontWeight: FontWeight.w800),
-                textAlign: TextAlign.center,
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      'RS - $pPrice',
-                      style: const TextStyle(
-                          color: Colors.red,
-                          fontSize: 20,
-                          fontStyle: FontStyle.normal,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      'Size - $pSize',
-                      style: const TextStyle(
-                          color: Colors.black,
-                          fontSize: 12,
-                          fontStyle: FontStyle.italic,
-                          fontWeight: FontWeight.normal),
-                    ),
-                  ],
+          child: Center(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 100,
+                  child: Image.network(pImage),
                 ),
-              )
-            ],
+                Text(
+                  pName,
+                  style: const TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontStyle: FontStyle.normal,
+                      fontWeight: FontWeight.w800),
+                  textAlign: TextAlign.center,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'RS - $pPrice',
+                        style: const TextStyle(
+                            color: Colors.red,
+                            fontSize: 20,
+                            fontStyle: FontStyle.normal,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        'Size - $pSize',
+                        style: const TextStyle(
+                            color: Colors.black,
+                            fontSize: 12,
+                            fontStyle: FontStyle.italic,
+                            fontWeight: FontWeight.normal),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
